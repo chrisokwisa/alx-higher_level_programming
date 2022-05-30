2360a66fc063725f6818f5fa667a7c858af2baa5
chrisokwisa <okwisachristabel@gmail.com>
2022-05-30 14:48:42 +0000

    commit

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/bin/python3
+str = "python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = [:363636363636363636363636363636363636363636363636363636363636363636363636] + str[15:] + str[:6]
+print(str)

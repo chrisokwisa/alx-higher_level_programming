6ec77aeef9215527339ea4fb5a03497147367690
chrisokwisa <okwisachristabel@gmail.com>
2022-05-31 14:02:24 +0000

    commit

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for alpha_letters in range(ord('a), ord('z')+):
+        if alpha_letters == 'e' or alpha_letters == 'q':
+        continue
+    print("{:c}".format(alpha_letter), end="")

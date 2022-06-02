70418483c25dc8c6ff5c4d648c542d100559cbc9
chrisokwisa <okwisachristabel@gmail.com>
2022-06-02 09:59:55 +0000

    addition

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     """print sum of 1 and 2"""
     from add_0 import add
-    
     a = 1
     b = 2
+    add = (a = 1) + (b = 1)
     print("{}"+"{}"=="{}".format(a, b, add(a + b)))

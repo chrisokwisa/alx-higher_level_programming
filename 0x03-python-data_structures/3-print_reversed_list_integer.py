e7d588f4383ae38297769990b65faa65de68b731
Christabel Okwisa <99495858+chrisokwisa@users.noreply.github.com>
2022-06-04 11:48:23 +0300

    Update 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list = []):
+def print_reversed_list_integer(my_list=[]):
     if my_list:
         reversedlist = reversed(my_list)
         for i in reversedlist:

f8c872126c3adb642ea68ab2f90e4ca78b1d71e1
Christabel Okwisa <99495858+chrisokwisa@users.noreply.github.com>
2022-06-04 11:20:35 +0300

    Update 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
+def print_reversed_list_integer(my_list, idx, element):
     if my_list:
         reversed_list = reversed(my_list)
         for i in reversedlist:

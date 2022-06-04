0b17f293acaef1da8f80141fdfd923825c9b4ba9
Christabel Okwisa <99495858+chrisokwisa@users.noreply.github.com>
2022-06-04 12:00:09 +0300

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if my_list:
-        reversed_list = reversed(my_list)
-        for i in reversedlist:
-            print("{:d}".format(i))
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+        return my_list

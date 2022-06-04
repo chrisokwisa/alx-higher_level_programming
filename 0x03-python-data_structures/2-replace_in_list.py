990f8b6b3d7a17e7bbbfb902011a91e855f308f9
chrisokwisa <okwisachristabel@gmail.com>
2022-06-04 08:08:01 +0000

    Function that replaces an element of a list at a specific position

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if my_list:
+        reversed_list = reversed(my_list)
+        for i in reversedlist:
+            print("{:d}".format(i))

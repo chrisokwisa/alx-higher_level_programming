77a4295280c82b39cc92c4cafcf1fc1af153a1ba
Christabel Okwisa <99495858+chrisokwisa@users.noreply.github.com>
2022-06-09 10:50:49 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    result = None
+    result = 0
     if a_dictionary:
         for key in a_dictionary:
             if a_dictionary[key] > result:

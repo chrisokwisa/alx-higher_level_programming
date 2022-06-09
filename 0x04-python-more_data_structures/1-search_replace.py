183d98cb1f3f17ce31a521c77533241c5428c80f
chrisokwisa <okwisachristabel@gmail.com>
2022-06-09 06:10:49 +0000

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-        return [replace if i == search else i for in my_list]
+    return [replace if i == search else i for i in my_list]

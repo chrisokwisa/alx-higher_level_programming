761d217221666c6a8b331635e309d340463dc704
chrisokwisa <okwisachristabel@gmail.com>
2022-06-09 05:52:35 +0000

    search and replace

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    if matrix:
-        return [i**2 for i in row[matrix]]
---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    return [replace if i == search else ifor in my_list]

a80b08b2f21a40edda6a3d2d7bd186bf6fd7469e
chrisokwisa <okwisachristabel@gmail.com>
2022-06-09 07:34:51 +0000

    Best scores

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    result None
+    if a_dictionary:
+        for key in a_dictionary:
+            if a_dictionary[key] > result:
+                result = a_dictionary[key]
+                winner = key
+        return the_winner

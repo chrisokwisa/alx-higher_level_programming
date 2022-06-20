c3893194a56ac340e77c7bebda146db3153ceb85
chrisokwisa <okwisachristabel@gmail.com>
2022-06-20 12:27:50 +0000

    Print integer error msg

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-def raise_exception_msg(message="")
-    raise NameError("{:s}".format(message))
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
+        return False

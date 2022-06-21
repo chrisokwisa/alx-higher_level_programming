7eadabc5a19f17d43220afff85cce2e82b2f967d
chrisokwisa <okwisachristabel@gmail.com>
2022-06-21 13:25:22 +0000

    Square the size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module creates a class named Square"""
+
+
+class Square:
+    """A class named Square
+    Attributes:
+    attr1 (size): size of square
+    """
+    def __init__(self, size):
+        """
+        Args:
+        size: size for __size attribute of class instance
+        """
+        self.__size = size

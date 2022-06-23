f00004e6ba3fe3e8eb287ae4848f4b61249521ef
chrisokwisa <okwisachristabel@gmail.com>
2022-06-23 07:44:57 +0000

    prints the addition of two integers and returns the result

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-for index in range(0, 26):
-    word = ord('z') - index
-    if (index % 2 == 1):
-        word = chr(word - ord('a') + ord('A'))
-    else:
-        word = chr(word)
-    print("{}".format(word), end='')
+for i in range(122, 96, -1):
+    print("{:c}".format(i - 32) if i % 2 != 0 else "{:c}".format(i), end="")

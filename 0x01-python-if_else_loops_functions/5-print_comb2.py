9d893479886b1b6949c9f07e92a831d4a7bd533e
chrisokwisa <okwisachristabel@gmail.com>
2022-06-23 07:27:56 +0000

    prints numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{:d} = 0x{:x}".format(i, i))
+for i in range(0, 100):
+    if i < 99:
+        print("{:02d}, ".format(i), end="")
+    else:
+        print("{:02d}".format(i))

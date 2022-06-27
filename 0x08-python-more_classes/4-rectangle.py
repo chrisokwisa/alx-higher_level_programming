c8d385a9f14221d00048a926912d57ecdffd136a
chrisokwisa <okwisachristabel@gmail.com>
2022-06-27 10:40:18 +0000

    Eval is magic

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -84,4 +84,4 @@ class Rectangle:
     def __repr__(self):
         """Returns the string representation of the class instance for
         recreation"""
-        return "Rectangle({:d})".format(self.__width, self__height)
+        return "Rectangle({:d}), {:d})".format(self.__width, self__height)

f4a773a9bdbceb23e78564e3976b35e6c9551896
chrisokwisa <okwisachristabel@gmail.com>
2022-06-27 11:34:26 +0000

    How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,18 +4,21 @@
 
 class Rectangle:
     """A class named Rectangle
-
     Attributes:
     attr1(width): width of rectangle
     attr2(height): height of rectangle
+    attr3(number_of_instances): number of instances
     """
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        type(self).number_of_instances += 1
 
     @property
     def height(self):
-        """Gets the height of the class istance"""
+        """Gets the height of the class instance"""
         return self.__height
 
     @height.setter
@@ -36,7 +39,7 @@ class Rectangle:
     def width(self, value):
         """Sets the width of the class instance"""
         if type(value) != int:
-            raise TypeError("Width must be an integer")
+            raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
@@ -46,10 +49,10 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-        """Returns the perimeter of the class instance"""
+        """Returns the permimeter of the class instance"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return (self._width * 2) + (self.__height * 2)
+        return (self.__width * 2) + (self.__height * 2)
 
     def __str__(self):
         """Returns the string representation of the class instance"""
@@ -68,6 +71,7 @@ class Rectangle:
         recreation"""
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
-    del __del__(self):
-        """Finalizes when instance is deleted"""
+    def __del__(self):
+        """Finalizer when instance is deleted"""
+        type(self).number_of_instances -= 1
         print("Bye rectangle...")

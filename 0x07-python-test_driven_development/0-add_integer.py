1c7e423385a23115699630913a6a141cb8edb01d
chrisokwisa <okwisachristabel@gmail.com>
2022-06-28 14:51:17 +0000

    commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines the add integer function
+"""
+
+def add_integer(a, b=98):
+    """Function that adds two string.
+    Args:
+         a (int): First addend.
+         b (int): Second addend, set to zero.
+    Returns:
+        int: The return value. The sum
+    """
+    if (type(a) != int and type(a) != float) or a != a:
+        raise TypeError("a must be an integer")
+    if type(b) != int and  type

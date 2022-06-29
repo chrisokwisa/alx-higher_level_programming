a2846257db9f1359c8196f9e1946066aa0bfd560
chrisokwisa <okwisachristabel@gmail.com>
2022-06-29 07:51:27 +0000

    Adds the integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,4 +13,6 @@ def add_integer(a, b=98):
     """
     if (type(a) != int and type(a) != float) or a != a:
         raise TypeError("a must be an integer")
-    if type(b) != int and type
+    if type(b) != int and type(b) != float or b != b:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)

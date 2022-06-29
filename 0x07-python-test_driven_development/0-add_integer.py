7d8e02f344eb87511ec48bc84fa7df45cf817133
chrisokwisa <okwisachristabel@gmail.com>
2022-06-29 07:56:48 +0000

    Adds the integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,10 +6,10 @@
 def add_integer(a, b=98):
     """Function that adds two string.
     Args:
-         a (int): First addend.
-         b (int): Second addend, set to zero.
+        a (int): First addend.
+        b (int): Second addend, set to zero
     Returns:
-        int: The return value. The sum
+         int: The return value. The sum
     """
     if (type(a) != int and type(a) != float) or a != a:
         raise TypeError("a must be an integer")

b17f054e437fdbae8f18049c681d555f41516c18
chrisokwisa <okwisachristabel@gmail.com>
2022-07-04 10:57:26 +0000

    kind of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module defines same class or inherit from"""
+
+
+de is_kind_of_class(obj, a_class):
+    """Returns True or False if kind class or not"""
+    return isinstance(obj, a_class)

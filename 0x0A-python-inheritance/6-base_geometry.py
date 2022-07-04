01ec6e04fa0f0f703e7ef793c28ec116b9a683d3
chrisokwisa <okwisachristabel@gmail.com>
2022-07-04 11:22:15 +0000

    Base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module defines a base_geometry"""
+
+
+class Base_geometry:
+    """A class base_geometry
+
+    Attributes:
+    attr1(area): Raises an exception
+    """
+    def area(self):
+        raise Exception("area() is not implemented")

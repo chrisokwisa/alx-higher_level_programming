69063f2daae37c0c18de44199bf57ab4e86b487e
chrisokwisa <okwisachristabel@gmail.com>
2022-07-04 10:38:43 +0000

    Task zero

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module creates a lookup function"""
+
+
+def lookup(obj):
+    """Returns the list of available attributes and methods of an object"""
+    return dir(obj)

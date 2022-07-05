7b480c3560073695f24f9683387bc31018e9ffe6
chrisokwisa <okwisachristabel@gmail.com>
2022-07-05 18:48:42 +0000

    addition of items

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module creates a script that adds args to python list and saves them"""
+
+
+from os import path
+from sys import argv
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+if path.exists("add_item.json") is false:
+    save_to_json_file([], "add_item.json")
+my_list = load_from_json_file("add_item.json")
+for i in range(1, len(argv)):
+    my_list.apped(argv[i])
+save_to_json_file(my_list, "add_item.json")

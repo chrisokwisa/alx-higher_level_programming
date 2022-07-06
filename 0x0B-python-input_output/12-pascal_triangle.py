060c04fc99b31b3339bcde7cbea73798d2c56b73
Christabel Okwisa <99495858+chrisokwisa@users.noreply.github.com>
2022-07-06 18:12:20 +0300

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+""" Module for pascal triangle """
+
+
 def pascal_triangle(n):
-    if n <= 0:
-        return []
-    pascal = []
-    row = []
-    for i in range(n):
-        row = [1]
-        if i > 0:
-            for j in range(i):
-                row.append(sum(pascal[-1][j:j+2]))
-        pascal.append(row)
-    return pascal
+    """ Pascal triangle """
+    tri = []
+    ini = 1
+    for i in range(0, n):
+        rows = []
+        for j in range(0, i + 1):
+            if i == 0 or j == 0 or (i > 0 and j == i):
+                rows.append(ini)
+            else:
+                rows.append(tri[i - 1][j] + tri[i - 1][j - 1])
+        tri.append(rows)
+    return tri

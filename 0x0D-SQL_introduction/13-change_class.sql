84c900dcbb2f08f29d972f0e9066691e33f9a7eb
chrisokwisa <okwisachristabel@gmail.com>
2022-08-09 14:59:40 +0000

    Score too low

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,3 +1,3 @@
--- computes the score average of all records
--- result column shoul be average
+-- removes all records with a score <= 5
+-- second_table of the database ht=btn_0c_0 in server
 DELETE FROM second_table WHERE score <= 5;

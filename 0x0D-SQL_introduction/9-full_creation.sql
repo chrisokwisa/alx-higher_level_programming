a39b314381c0d28c1d01dbf44bdbd81dc6ff629b
chrisokwisa <okwisachristabel@gmail.com>
2022-08-09 12:05:42 +0000

    creates a full table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- creates a table
+-- creates a second table in the database and adds multiple rows
+CREATE TABLE IF NOT EXISTS second table(id INT DEFAULT NULL,
+name VARCHAR(256),
+score INT);
+INSERT INTO `second_table`(`id`, `name`, `score`)
+VALUES
+	(1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8);

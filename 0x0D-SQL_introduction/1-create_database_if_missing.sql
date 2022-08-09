fd680df3168abe11c625a925c259c51d25e4423b
chrisokwisa <okwisachristabel@gmail.com>
2022-08-09 11:24:13 +0000

    creates a database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
---creates the database in MYSQL server
---if database already exists script should not fail
+-- creates the database in MYSQL server
+-- if database already exists script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;

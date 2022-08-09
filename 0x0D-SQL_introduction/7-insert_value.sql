6477f7bda0045b6def4bc86f34aaf13408c370cd
chrisokwisa <okwisachristabel@gmail.com>
2022-08-09 11:39:02 +0000

    Inserts values

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- inserts a value
 -- inserts a row in the table
-INSERT INTO first_table(id, Best School)VALUES(89, "Best School");
+INSERT INTO `first_table`(`id`, `name`)VALUES(89, "Best School");

dedc76b89bab88ad01d2c0c91437c75105f668fb
chrisokwisa <okwisachristabel@gmail.com>
2022-08-09 13:07:35 +0000

    Select the best

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 -- lists records with a score >= 10
 -- database htbn_0c_0 in Mysql server
-SELECT score name FROM second_table WHERE score >= 10 ORDER BY DESC; 
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY DESC; 
 

47b29763772f96a3e99d0fb2e6c782b96348468d
chrisokwisa <okwisachristabel@gmail.com>
2022-09-05 13:20:44 +0000

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,6 +4,6 @@ if (isNaN(parseInt(process.argv[2], 10))) {
 } else {
   let i;
   for (i = 0; i < process.argv[2]; i++) {
-   console.log('X'.repeat(process.argv[2]));
+    console.log('X'.repeat(process.argv[2]));
   }
 }

5caf6c8ad496497f489dcea7f2abe40d4bb06710
chrisokwisa <okwisachristabel@gmail.com>
2022-09-05 13:09:59 +0000

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (isNaN(parseInt(process.argv[2], 10))) {
-  console.log('Missing number of occurences');
+  console.log('Missing number of occurrences');
 } else {
   let i;
   for (i = 0; i < process.argv[2]; i++) {

1c80059a2cca476252bb1db0ae1fecdf146776a8
chrisokwisa <okwisachristabel@gmail.com>
2022-09-05 12:56:03 +0000

    To integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (isNaN(parseInt(process.argv[2], 10))) {
-  console.log('Not a Number');
+  console.log('Not a number');
 } else {
   console.log('My number:', parseInt(process.argv[2], 10));
 }

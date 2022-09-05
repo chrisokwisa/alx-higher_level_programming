8acf50a1cd15f1b99eb3340791d0f71842173b06
chrisokwisa <okwisachristabel@gmail.com>
2022-09-05 12:18:50 +0000

    Arguments

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const myVar = 'C is fun\n' + 'Python is cool\n' + 'JavaScript is amazing'
+const myVar = 'C is fun\n' + 'Python is cool\n' + 'JavaScript is amazing';
 console.log(myVar);

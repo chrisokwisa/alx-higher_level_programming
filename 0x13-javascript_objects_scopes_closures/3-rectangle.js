51aea1e8169ce2c79aa79564528895d543101c07
chrisokwisa <okwisachristabel@gmail.com>
2022-09-06 14:36:14 +0000

    task three

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.height = w;
-      this.width = h;
+    constructor (w, h) {
+        if (w > 0 && h > 0) {
+            this.width = w;
+            this.height = h;
+        }
+    }
+}
+
+print () {
+    let i = 0;
+    for (i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
-
-   print () {
-      let i = 0;
-      for (i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
-   }
 };

71c8dbe8ad3ed2583d3a9d938f0165b79c804322
chrisokwisa <okwisachristabel@gmail.com>
2022-09-07 17:58:07 +0000

    task four

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,23 +2,23 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
-       this.width = w;
-       this.height = h;
+      this.width = w;
+      this.height = h;
     }
   }
 
-    print () {
-       let i = 0;
-       for (i = 0; i < this.height; i++) {
-	   console.log('X'.repeat(this.width));
-       }
+  print () {
+    let i = 0;
+    for (i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
+  }
 
-    double () {
-      [this.width, this.height] = [this.width * 2, this.height * 2];
-    }
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2];
+  }
 
-    rotate () {
-      [this.width, this.height] = [this.height, this.width];
-    }
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
 };

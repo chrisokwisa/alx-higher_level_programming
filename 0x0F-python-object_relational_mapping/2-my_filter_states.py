ee85ec29fbfa68b786ad14a7f9db82e725952c81
chrisokwisa <okwisachristabel@gmail.com>
2022-09-09 14:41:50 +0000

    filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module takes in an argument and display all values in table"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute(
+            "SELECT * FROM states WHERE Name LIKE BINARY '{}' ORDER BY id ASC"
+            .format(sys.argv[4]))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()

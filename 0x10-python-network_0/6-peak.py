33424f622c080b3c2e595b6779a5f5b43b8d6c69
chrisokwisa <okwisachristabel@gmail.com>
2022-09-22 10:38:36 -0700

    Technical interview question

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module contains a script thatfinds a peak in list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    """Finds a peak in a list of unsorted integers"""
+    if list_of_integers == []:
+        return None
+
+    length = len(list_of_integers)
+    if length == 1:
+        return list_of_integers[0]
+    elif length == 2:
+        return max(list_of_integers)
+
+    mid = int(length / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1])

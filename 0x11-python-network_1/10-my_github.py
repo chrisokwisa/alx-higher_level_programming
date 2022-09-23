a0d107d2a3ef2d7b32bc9340a50de857914dd66a
chrisokwisa <okwisachristabel@gmail.com>
2022-09-23 05:31:41 -0700

    Takes your github credentials

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Takes your Github credentials and uses 
+the Github API to displays my id
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    username = argv[1]
+    password =argv[2]
+    try:
+        r = request.get('https://api.github.com/user', auth=(argv[1], argv[2])).json()
+        print(r.get('id'))
+    except:
+        print("None")

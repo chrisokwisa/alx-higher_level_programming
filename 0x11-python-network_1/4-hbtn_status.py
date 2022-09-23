602c717998ca35866988ac649ee56a70b71db088
chrisokwisa <okwisachristabel@gmail.com>
2022-09-23 05:10:53 -0700

    status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,6 +2,7 @@
 """fetches https://alx-intranet.hbtn.io/status"""
 import requests
 
+
 if __name__ == "__main__":
     r = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")

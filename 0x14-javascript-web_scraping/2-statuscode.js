945b1ada104a1ea0bb1929d57f17542c90ce5b54
chrisokwisa <okwisachristabel@gmail.com>
2022-10-18 09:13:16 +0000

    commit

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,8 +3,8 @@ const request = require('request');
 
 request(process.argv[2], function (error, response) {
   if (error) {
-    console.error('error: ', error);
+    console.error('error:', error);
     return;
   }
-  console.log('code: ', response && response.statuscode);
+  console.log('code:', response && response.statuscode);
 });

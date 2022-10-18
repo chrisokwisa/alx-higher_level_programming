af9f48da13e251e5c444d856763ea844cacc6d8a
chrisokwisa <okwisachristabel@gmail.com>
2022-10-18 08:29:20 +0000

    writes a string to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});

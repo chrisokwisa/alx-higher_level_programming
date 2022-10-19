e2db5e31095c7ebc81ea9672e5d03552fc3e56b9
chrisokwisa <okwisachristabel@gmail.com>
2022-10-19 09:31:40 +0000

    displays the status cocde of a GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
+
 const request = require('request');
-request(process.argv[2], function (error, response) {
-  if (error) {
-    console.error('error:' error);
-  } else {
-    console.log('code:', response && response.statuscode);
-  }
-});
+const url = process.argv[2];
+request
+  .get(url)
+  .on('response', function (response) {
+    console.log('code: ' + response.statusCode);
+  });

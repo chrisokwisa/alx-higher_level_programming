fed17564c638ff565194208d33de4a8b66301d7e
chrisokwisa <okwisachristabel@gmail.com>
2022-10-19 10:42:24 +0000

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,4 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-
-9i
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+let resultDict = {};
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    resultDict = JSON.parse(body);
+    console.log(resultDict.title);
+  }
+});
